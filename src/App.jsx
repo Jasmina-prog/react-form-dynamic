da054980508382258650526fdfb0380b443ec5ad
Jasmina <jasmina@Jasminas-MacBook-Air.local>
2024-09-27 18:17:11 +0500

    error decorations appeared

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import FormDynamic from './FormDynamic'
+
+function App() {
+
+  return (
+    <>
+    <FormDynamic/>
+    </>
+  )
+}
+
+export default App
---
+++ src/FormDynamic.jsx
@@ -0,0 +1,99 @@
+import { Button, Form, Input, Select, InputNumber } from 'antd';
+import { useForm, Controller, useFieldArray } from 'react-hook-form';
+function FormDynamic() {
+    const { handleSubmit, control } = useForm()
+
+    useForm({
+        defaultValues: {
+            phoneNumber:[ { number: ''}  ]
+        }
+    })
+    const submitFn = (data) => {
+        console.log("data: ", data);
+
+    }
+
+    const {fields, append, remove} = useFieldArray({
+        name:'phoneNumber',
+        control
+    })
+
+    const regionData = [
+        {
+            value: 1,
+            label: 'Tashkent'
+        },
+        {
+            value: 2,
+            label: 'Andijon'
+        },
+        {
+            value: 3,
+            label: 'Qashqadaryo'
+        },
+        {
+            value: 4,
+            label: 'Samarqand'
+        },
+        {
+            value: 5,
+            label: 'Buxoro'
+        }
+    ]
+    return (
+        <>
+            <Form >
+                <Controller
+                    name='name'
+                    control={control}
+                    render={({ field }) => (
+                        <Input {...field}
+                            placeholder='Enter your name' />
+                    )}
+                />
+                <Controller
+                name='regions'
+                control={control}
+                render={({ field }) =>(
+                    <Select {...field} options={regionData}/>
+                )}
+                />
+
+                <Controller
+                name='phone'
+                control={control}
+                render={({ field }) => (
+                    <InputNumber {...field}/>
+                )}/>
+
+                <div>
+                    hi
+                    {fields.map((field, index) => {
+                        return(
+                            <div className="form-control" key={field.id}>
+                                <Controller
+                                name={`phoneNumber.${index}.number`}
+                                control
+                                render={({ field }) => (
+                                    <Input {...field}/>
+                                )}
+                                />
+                                {
+                                    index > 0 && (
+                                        <Button onClick={()=> remove(index)}> Remove </Button>
+                                    )
+                                }
+                            </div>
+                        )
+                    })}
+                    <Button onClick={()=> append({ number: "" })}> Add </Button>
+                </div>
+                <Button type="primary" htmlType="submit" onClick={handleSubmit(submitFn)}>
+                    Submit
+                </Button>
+            </Form>
+        </>
+    )
+}
+
+export default FormDynamic
